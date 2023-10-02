7bd71d8c4c409c08e76bf57fee818fd0157f6db8
Redet Getachew <redetg@gmail.com>
2023-10-02 13:28:24 +0300

    First commit

---
+++ src/main/java/com/habtech/ETLHabtech/EtlHabtechApplication.java
@@ -0,0 +1,13 @@
+package com.habtech.ETLHabtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlHabtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlHabtechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habtech/ETLHabtech/EtlHabtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habtech.ETLHabtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlHabtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
